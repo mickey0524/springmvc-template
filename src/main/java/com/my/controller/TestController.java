a4a2c4422dee380c186a00227f43dc753008baa7
mickey0524 <buptbh@163.com>
2019-11-19 22:08:42 +0800

    feat(springmvc): udpate springmvc template

--- src/main/java/com/my/controller/TestController.java
+++ src/main/java/com/my/controller/TestController.java
@@ -10,12 +10,27 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
     @RequestMapping(value = "/test_get", method = RequestMethod.GET)
-    public ResponseEntity test() {
+    public ResponseEntity test_get() {
         return ResponseEntity.ok();
     }
 
+    @RequestMapping(value = "/test_get_param", method = RequestMethod.GET)
+    public ResponseEntity test_get(@RequestParam(value = "id", defaultValue = "10") long id) {
+        return ResponseEntity.successWithData(id);
+    }
+
+    @RequestMapping(value = "/test_get_param_v1", method = RequestMethod.GET)
+    public ResponseEntity test_get(IdDto idDto) {
+        return ResponseEntity.successWithData(idDto.getId());
+    }
+
+    @RequestMapping(value = "/test_get_path/{id}", method = RequestMethod.GET)
+    public ResponseEntity test_get_path(@PathVariable("id") long id) {
+        return ResponseEntity.successWithData(id);
+    }
+
     @RequestMapping(value = "/test_post", method = RequestMethod.POST)
-    public ResponseEntity<Student> test(@RequestBody IdDto idDto) {
+    public ResponseEntity<Student> test_post(IdDto idDto) {
         return ResponseEntity.successWithData(new Student("test"));
     }
 }
